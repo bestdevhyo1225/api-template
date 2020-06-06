9d3de16b4efacdcb5db604a9f35689c5297ce21b
janghyoseog <bestdevhyo1225@gmail.com>
2020-06-06 18:53:01 +0900

    test: 테스트 코드 수정

--- test/unit/data/typeOrmUserRepository.test.ts
+++ test/unit/data/typeOrmUserRepository.test.ts
@@ -1,6 +1,6 @@
 describe('Data :: TypeOrmUserRepository 클래스 테스트', () => {
   test('init', async () => {
-    const productId: number = 1;
-    expect(productId).toBe(1);
+    const userId: number = 1;
+    expect(userId).toBe(1);
   });
 });
